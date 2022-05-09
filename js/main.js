4910104621712dbd4f9f3001a69737ac0901582e
soxrobjumayev <soxrobjumayev>
2022-05-10 02:31:47 +0500

    3.6

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var info = {}
+alert("Aliwerboy Dubaiga ketmoqchi")
+alert("30.000.000 so'm puli bor")
+alert("yo'l haqi 500$     SO'MDA esa ")
+var a =500
+var b = 9433
+alert(a * b)
+
+alert("mehmonxona to'lovi 200$    SO'MDA esa")
+var a =250
+var b = 9433
+alert(a * b)
+
+alert("sayr ucun  120 yevro    SO'MDA esa")
+var a =120
+var b = 10354
+alert(a * b)
+
+alert("hamma harajatlar so'mda ")
+var a = 4716500
+var b = 2358250
+var d = 1242480
+alert(a + b +d)
